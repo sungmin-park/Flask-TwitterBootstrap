2bc800cf4689bf587bf971cb0836c4b20d1f03d4
Sung Min Park <vamf12@gmail.com>
2013-09-03 17:55:07 +0900

    add MANIFEST for template mixins, ignore dist folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-TwitterBootstrap',
-    version='0.0.1',
+    version='0.0.2',
     packages=['flask_twitterbootstrap'],
     include_package_data=True,
     zip_safe=False,
